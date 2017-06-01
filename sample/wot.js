4f44b883e2af1e57bb948064072a5e9ffc6ae5f1
lgtkaushalya <lgtkaushalya@gmail.com>
2017-06-02 00:08:02 +0530

    included the chat with firebase

---
+++ sample/wot.js
@@ -0,0 +1,62 @@
+(function() {
+  'use strict';
+
+  angular.module('app', ['irontec.simpleChat']);
+
+  angular.module('app').controller('Shell', Shell);
+
+  function Shell() {
+
+    var vm = this;
+    vm.database = setupFirebase();
+    vm.messages = [];
+
+    var chats = vm.database.ref('/chats');
+    chats.on('value', function(updatedChats) {
+	vm.messages.length = 0;
+	updatedChats.forEach(function(message) {
+	    var value = message.val();
+	    vm.messages.push({
+		'username': value.username,
+       		'content': value.content
+	    });
+	});
+	
+    });
+
+    vm.username = 'Matt';
+
+    vm.sendMessage = function(message, username) {
+      if(message && message !== '' && username) {
+        vm.messages.push({
+          'username': username,
+          'content': message
+        });
+      }
+    };
+
+    vm.syncMessage = function(message, username) {
+	var chat = vm.database.ref('/chats').push();
+	chat.set({
+    	    'username': username,
+    	    'content': message
+ 	});
+    };
+
+    function setupFirebase() {
+	    var config = {
+	apiKey: "AIzaSyAgWGcekF_2zb2wOajz15J4TLWl-e-NaN4",
+	authDomain: "orange-wot.firebaseapp.com",
+	databaseURL: "https://orange-wot.firebaseio.com",
+	projectId: "orange-wot",
+	storageBucket: "orange-wot.appspot.com",
+	messagingSenderId: "785108208926"
+	    };
+
+	    firebase.initializeApp(config);
+	return firebase.database();
+    }
+
+  }
+
+})();
